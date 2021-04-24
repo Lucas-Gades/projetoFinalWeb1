aefb2d9e482962d29042ff3a90e19833a6c17a94
Lucas Gades <lucasgades@hotmail.com>
2021-04-23 22:30:18 -0300

    ok

--- view/listagemview.php
+++ view/listagemview.php
@@ -15,10 +15,11 @@
                 $timeJogador = $jogador->getTimeJogador();
                 
                 echo "
-                <aside col-md-3>
-                    <div class='card col-10 col-md-3 col-sm-6 ml-2 p-0 mt-5' style='width: 26rem; background: yellow; border: 5px solid darkgreen; margin-bottom:20px;'>
+                <div>
+                 <aside class='col-md-3 col-12 '>
+                    <div class='card ml-2 p-0 mt-5' style='width: 18rem; background-color: yellow; border: 5px solid darkgreen; margin-bottom:20px;'>
                         <h2 class='text-center' style='background-color: rgba(0, 127, 247, 0.74); margin-bottom: ;'>$nome</h2>
-                        <img src='../img/$foto' style='border:2px solid black; height:240px; width:100%;' class='' alt='...''>
+                        <img src='../img/$foto' style='border:2px solid black; height:240px; width:100%;' class='' alt='foto do jogador $nome'>
                     <div class='card-body'>
                         <p class='card-text' style='margin-top:10px;'><span style='font-weight: bold;'>Nome: </span>$nome</p>
                         <p class='card-text'><span style='font-weight: bold;'>Idade: </span>$idade anos</p>
@@ -30,7 +31,8 @@
                         </div>
                     </div>
                     </div>
-                </aside>    
+                 </aside>
+                </div>    
                 ";
             }
         }
